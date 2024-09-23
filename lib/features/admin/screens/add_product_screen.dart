b55d7aeeb4ed102e013da2a33a39f30ed593d491
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-09-23 12:33:37 +0200

    admin add prod service done

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -4,6 +4,7 @@ import 'package:amazon_clone_tutorial/common/widgets/custom_button.dart';
 import 'package:amazon_clone_tutorial/common/widgets/custom_textfield.dart';
 import 'package:amazon_clone_tutorial/constants/global_variables.dart';
 import 'package:amazon_clone_tutorial/constants/utils.dart';
+import 'package:amazon_clone_tutorial/features/admin/services/admin_services.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,7 @@ class _AddProductScreenState extends State<AddProductScreen> {
   final TextEditingController descriptionController = TextEditingController();
   final TextEditingController priceController = TextEditingController();
   final TextEditingController quantityController = TextEditingController();
-  // final AdminServices adminServices = AdminServices();
+  final AdminServices adminServices = AdminServices();
 
   String category = 'Mobiles';
   List<File> images = [];
@@ -45,17 +46,17 @@ class _AddProductScreenState extends State<AddProductScreen> {
   ];
 
   void sellProduct() {
-    // if (_addProductFormKey.currentState!.validate() && images.isNotEmpty) {
-    //   adminServices.sellProduct(
-    //     context: context,
-    //     name: productNameController.text,
-    //     description: descriptionController.text,
-    //     price: double.parse(priceController.text),
-    //     quantity: double.parse(quantityController.text),
-    //     category: category,
-    //     images: images,
-    //   );
-    // }
+    if (_addProductFormKey.currentState!.validate() && images.isNotEmpty) {
+      adminServices.sellProduct(
+        context: context,
+        name: productNameController.text,
+        description: descriptionController.text,
+        price: double.parse(priceController.text),
+        quantity: double.parse(quantityController.text),
+        category: category,
+        images: images,
+      );
+    }
   }
 
   void selectImages() async {
