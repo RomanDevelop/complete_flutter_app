a0fb1fd5c66ffaecf15a48e8b628c90d554a2a62
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-09-25 14:32:53 +0200

    orders UI, service done

--- lib/features/address/screens/address_screen.dart
+++ lib/features/address/screens/address_screen.dart
@@ -214,7 +214,7 @@ class _AddressScreenState extends State<AddressScreen> {
                   onPaymentResult: onGooglePayResult,
                   paymentItems: paymentItems,
                   height: 50,
-                  // style: GooglePayButtonStyle.black,
+                  //  style: GooglePayButtonStyle.black,
                   type: GooglePayButtonType.buy,
                   margin: const EdgeInsets.only(top: 15),
                   loadingIndicator: const Center(
